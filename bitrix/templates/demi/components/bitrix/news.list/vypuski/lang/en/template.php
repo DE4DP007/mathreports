ae27a851d38527a6f1952bfc70733826e9c884fa
Saarus <sm.saarus@gmail.com>
2018-03-01 15:01:06 +0300

    Заменил отображение списка выпусков компонентом

---
+++ bitrix/templates/demi/components/bitrix/news.list/vypuski/lang/en/template.php
@@ -0,0 +1,6 @@
+<?
+$MESS["CT_BNL_ELEMENT_DELETE_CONFIRM"] = "All the information linked to this record will be deleted. Continue anyway?";
+$MESS["BLOCK_ID_NUM"] = 14;
+$MESS["NUMBER_OF_ARTICLE"] = "Number of articles";
+$MESS["TOTAL_PAGES"] = "Total pages";
+?>
