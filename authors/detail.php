308e707ee77488529279df34d939703ed403bab9
Saarus <sm.saarus@gmail.com>
2018-03-02 12:16:01 +0300

    update authors detail pages

--- authors/detail.php
+++ authors/detail.php
@@ -1,17 +1,9 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");?>
 
-<?if (SITE_ID == "s1") {
-	$fname = "FNAME";
-	$add_info = "ADD_INFO";
-	$APPLICATION->SetTitle("Сведения об авторе");
-} else {
-	$fname = "FNAME_EN";
-	$add_info = "ADD_INFO_EN";
-	$APPLICATION->SetTitle("About the author");
-}?>
+<?$APPLICATION->SetTitle("Сведения об авторе");?>
 
 <h3 class="journhead text-right margtop15">
-    <?echo (SITE_ID == "s1" ? "Сведения об авторе" : "About the author")?>
+    Сведения об авторе
 </h3><hr>
 
 
@@ -57,8 +49,8 @@
 		"PAGER_TEMPLATE" => ".default",
 		"PAGER_TITLE" => "Страница",
 		"PROPERTY_CODE" => array(
-			0 => $fname,
-			1 => $add_info,
+			0 => "FNAME",
+			1 => "ADD_INFO",
 			2 => "",
 			3 => "",
 			4 => "",
@@ -81,5 +73,90 @@
 	false
 );?>
 
+<?$arFilter = Array("PROPERTY_AUTHORS" => getCurrentID(21, $_REQUEST["ELEMENT_CODE"]));?>
+        <h2 class="text-center">Статьи автора в Журнале</h2><br/>
+
+    <?$APPLICATION->IncludeComponent("bitrix:news.list", "authors_articles", Array(
+	"ACTIVE_DATE_FORMAT" => "d.m.Y",	// Формат показа даты
+		"ADD_SECTIONS_CHAIN" => "Y",	// Включать раздел в цепочку навигации
+		"AJAX_MODE" => "N",	// Включить режим AJAX
+		"AJAX_OPTION_ADDITIONAL" => "",	// Дополнительный идентификатор
+		"AJAX_OPTION_HISTORY" => "N",	// Включить эмуляцию навигации браузера
+		"AJAX_OPTION_JUMP" => "N",	// Включить прокрутку к началу компонента
+		"AJAX_OPTION_STYLE" => "Y",	// Включить подгрузку стилей
+		"CACHE_FILTER" => "N",	// Кешировать при установленном фильтре
+		"CACHE_GROUPS" => "Y",	// Учитывать права доступа
+		"CACHE_TIME" => "36000000",	// Время кеширования (сек.)
+		"CACHE_TYPE" => "A",	// Тип кеширования
+		"CHECK_DATES" => "Y",	// Показывать только активные на данный момент элементы
+		"COMPONENT_TEMPLATE" => "issue_articles",
+		"DETAIL_URL" => "",	// URL страницы детального просмотра (по умолчанию - из настроек инфоблока)
+		"DISPLAY_BOTTOM_PAGER" => "Y",	// Выводить под списком
+		"DISPLAY_DATE" => "Y",	// Выводить дату элемента
+		"DISPLAY_NAME" => "Y",	// Выводить название элемента
+		"DISPLAY_PICTURE" => "Y",	// Выводить изображение для анонса
+		"DISPLAY_PREVIEW_TEXT" => "Y",	// Выводить текст анонса
+		"DISPLAY_TOP_PAGER" => "N",	// Выводить над списком
+		"FIELD_CODE" => array(	// Поля
+			0 => "",
+			1 => "",
+		),
+		"FILTER_NAME" => "arFilter",	// Фильтр
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",	// Скрывать ссылку, если нет детального описания
+		"IBLOCK_ID" => "17",	// Код информационного блока
+		"IBLOCK_TYPE" => "issues",	// Тип информационного блока (используется только для проверки)
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",	// Включать инфоблок в цепочку навигации
+		"INCLUDE_SUBSECTIONS" => "N",	// Показывать элементы подразделов раздела
+		"MESSAGE_404" => "",	// Сообщение для показа (по умолчанию из компонента)
+		"NEWS_COUNT" => "20",	// Количество новостей на странице
+		"PAGER_BASE_LINK_ENABLE" => "N",	// Включить обработку ссылок
+		"PAGER_DESC_NUMBERING" => "N",	// Использовать обратную навигацию
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",	// Время кеширования страниц для обратной навигации
+		"PAGER_SHOW_ALL" => "N",	// Показывать ссылку "Все"
+		"PAGER_SHOW_ALWAYS" => "N",	// Выводить всегда
+		"PAGER_TEMPLATE" => ".default",	// Шаблон постраничной навигации
+		"PAGER_TITLE" => "Новости",	// Название категорий
+		"PARENT_SECTION" => "",	// ID раздела
+		"PARENT_SECTION_CODE" => "",	// Код раздела
+		"PREVIEW_TRUNCATE_LEN" => "",	// Максимальная длина анонса для вывода (только для типа текст)
+		"PROPERTY_CODE" => array(	// Свойства
+			0 => "START_PAGE",
+			1 => "END_PAGE",
+			2 => "AUTHORS",
+			3 => "",
+		),
+		"SET_BROWSER_TITLE" => "N",	// Устанавливать заголовок окна браузера
+		"SET_LAST_MODIFIED" => "N",	// Устанавливать в заголовках ответа время модификации страницы
+		"SET_META_DESCRIPTION" => "N",	// Устанавливать описание страницы
+		"SET_META_KEYWORDS" => "Y",	// Устанавливать ключевые слова страницы
+		"SET_STATUS_404" => "N",	// Устанавливать статус 404
+		"SET_TITLE" => "N",	// Устанавливать заголовок страницы
+		"SHOW_404" => "N",	// Показ специальной страницы
+		"SORT_BY1" => "ACTIVE_FROM",	// Поле для первой сортировки новостей
+		"SORT_BY2" => "SORT",	// Поле для второй сортировки новостей
+		"SORT_ORDER1" => "DESC",	// Направление для первой сортировки новостей
+		"SORT_ORDER2" => "ASC",	// Направление для второй сортировки новостей
+	),
+	false
+);?>
+
+<p class="col-md-12 text-left">
+    <a href="<?=SITE_DIR?>authors/" class="btn btn-lg btn-primary">
+        <span class="glyphicon glyphicon-arrow-left"></span>
+        В список авторов
+    </a>
+</p>
+
+
+
+<?function getCurrentID($iblock_id, $code) {
+    if(CModule::IncludeModule("iblock")) {
+        $arFilter = array("IBLOCK_ID"=>$iblock_id, "CODE" => $code);
+        $res = CIBlockElement::GetList(array(), $arFilter, false, array("nPageSize"=>1), array('ID'));
+        $element = $res->Fetch();
+        if($res->SelectedRowsCount() != 1) return '<p>Элемент не найден</p>';
+        else return $element['ID'];
+    }
+}?>
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ bitrix/templates/demi/components/bitrix/news.list/authors_articles/lang/en/template.php
@@ -0,0 +1,5 @@
+<?
+$MESS["CT_BNL_ELEMENT_DELETE_CONFIRM"] = "All the information linked to this record will be deleted. Continue anyway?";
+$MESS["AUTHOR_NAME"] = "FNAME_EN";
+$MESS["PAGES"] = "Pp.";
+?>
